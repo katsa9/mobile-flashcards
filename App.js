d65ad8df344b046c1a8051eadd79861ada6372f6
katsa9 <kathleen.brand2@gmail.com>
2019-10-02 11:24:32 +0200

    dev: add colors and tab navigation

--- App.js
+++ App.js
@@ -1,14 +1,70 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import DeckList from './components/DeckList'
+import NewDeck from './components/NewDeck'
+import { createBottomTabNavigator } from 'react-navigation-tabs' 
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { FontAwesome } from '@expo/vector-icons'
+import { purple, white, lightPurp } from './utils/colors'
+
+const RouteConfigs = {
+  DeckList: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'My Decks',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='th-list' size={30} color={tintColor} />
+    },
+  },
+  NewDeck: {
+    screen: NewDeck,
+    navigationOptions: {
+      tabBarLabel: 'New Deck',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    },
+  }
+}
+
+const TabNavigatorConfig = {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor:  purple,
+    style: {
+      height: 56,
+      backgroundColor: white,
+      shadowColor: "rgba(0, 0, 0, 0.24)",
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+};
+const TabNavigator = createBottomTabNavigator(RouteConfigs, TabNavigatorConfig);
+const AppContainer = createAppContainer(TabNavigator);
+
+function UdaciStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: 50 }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <DeckList />
+   <View style={{flex:1}}> 
+        <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
+      <AppContainer />
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ utils/colors.js
@@ -0,0 +1,8 @@
+export const purple = '#292477'
+export const gray = '#757575'
+export const white = '#fff'
+export const red = '#E33E3E'
+export const orange = '#f26f28'
+export const blue = '#4e4cb8'
+export const lightPurp = '#7c53c3'
+export const green = '#79E355'
